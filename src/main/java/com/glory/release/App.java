215b1c86fe86637e857c41c9a96ab49d916c0c96
UyiGodfrey <uyigodfrey23@gmail.com>
2026-07-25 13:12:46 +0100

    Initial Maven release pipeline

---
+++ src/main/java/com/glory/release/App.java
@@ -0,0 +1,10 @@
+package com.glory.release;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/glory/release/AppTest.java
@@ -0,0 +1,19 @@
+package com.glory.release;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
